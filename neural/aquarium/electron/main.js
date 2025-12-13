0b2b362e0f518b75b88a5b8a769a607b97f1058f
Kuro <Lemniscate_zero@proton.me>
2025-12-13 21:36:43 +0000

    Add packaging, distribution, and CI/CD configuration for cross-platform releases with auto-update and code signing

--- neural/aquarium/electron/main.js
+++ neural/aquarium/electron/main.js
@@ -1,95 +1,259 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, Menu, dialog, ipcMain } = require('electron');
+const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
 const path = require('path');
-const WindowManager = require('./window/window-manager');
-const IPCController = require('./ipc/ipc-controller');
-const MenuBuilder = require('./menu/menu-builder');
-const ConfigManager = require('./config/config-manager');
+const isDev = require('electron-is-dev');
 
-class AquariumIDE {
-  constructor() {
-    this.windowManager = null;
-    this.ipcController = null;
-    this.menuBuilder = null;
-    this.configManager = null;
-    this.mainWindow = null;
-  }
+log.transports.file.level = 'info';
+autoUpdater.logger = log;
 
-  async initialize() {
-    this.configManager = new ConfigManager();
-    await this.configManager.initialize();
+let mainWindow;
+let updateCheckInterval;
 
-    this.windowManager = new WindowManager(this.configManager);
-    this.ipcController = new IPCController(this.configManager);
-    this.menuBuilder = new MenuBuilder(this.configManager);
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 800,
+    minHeight: 600,
+    icon: path.join(__dirname, '../build/icon.png'),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+      preload: path.join(__dirname, 'preload.js')
+    },
+    backgroundColor: '#1e1e1e',
+    show: false,
+    title: 'Neural Aquarium'
+  });
 
-    this.setupAppEventHandlers();
-    this.setupIPCHandlers();
-  }
+  const startUrl = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`;
 
-  setupAppEventHandlers() {
-    app.on('ready', async () => {
-      await this.createMainWindow();
-    });
+  mainWindow.loadURL(startUrl);
 
-    app.on('window-all-closed', () => {
-      if (process.platform !== 'darwin') {
-        app.quit();
-      }
-    });
-
-    app.on('activate', async () => {
-      if (BrowserWindow.getAllWindows().length === 0) {
-        await this.createMainWindow();
-      }
-    });
-
-    app.on('before-quit', () => {
-      this.configManager.save();
-    });
-  }
-
-  setupIPCHandlers() {
-    this.ipcController.registerHandlers();
-  }
-
-  async createMainWindow() {
-    this.mainWindow = await this.windowManager.createMainWindow();
-    
-    const menu = this.menuBuilder.buildMenu(this.mainWindow);
-    this.mainWindow.setMenu(menu);
-
-    this.mainWindow.on('closed', () => {
-      this.mainWindow = null;
-    });
-
-    if (process.env.NODE_ENV === 'development') {
-      this.mainWindow.webContents.openDevTools();
-    }
-
-    this.loadApplication();
-  }
-
-  loadApplication() {
-    const isDev = process.env.NODE_ENV === 'development';
-    
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
     if (isDev) {
-      this.mainWindow.loadURL('http://localhost:3000');
-    } else {
-      this.mainWindow.loadFile(path.join(__dirname, 'renderer', 'index.html'));
+      mainWindow.webContents.openDevTools();
     }
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+    if (updateCheckInterval) {
+      clearInterval(updateCheckInterval);
+    }
+  });
+
+  createMenu();
+
+  if (!isDev) {
+    checkForUpdates();
+    updateCheckInterval = setInterval(() => {
+      checkForUpdates();
+    }, 1000 * 60 * 60);
   }
 }
 
-const aquarium = new AquariumIDE();
+function createMenu() {
+  const template = [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'New Project',
+          accelerator: 'CmdOrCtrl+N',
+          click: () => {
+            mainWindow.webContents.send('menu-new-project');
+          }
+        },
+        {
+          label: 'Open Project',
+          accelerator: 'CmdOrCtrl+O',
+          click: () => {
+            mainWindow.webContents.send('menu-open-project');
+          }
+        },
+        {
+          label: 'Save',
+          accelerator: 'CmdOrCtrl+S',
+          click: () => {
+            mainWindow.webContents.send('menu-save');
+          }
+        },
+        { type: 'separator' },
+        {
+          label: 'Exit',
+          accelerator: 'CmdOrCtrl+Q',
+          click: () => {
+            app.quit();
+          }
+        }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'selectAll' }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forceReload' },
+        { role: 'toggleDevTools' },
+        { type: 'separator' },
+        { role: 'resetZoom' },
+        { role: 'zoomIn' },
+        { role: 'zoomOut' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'Documentation',
+          click: async () => {
+            const { shell } = require('electron');
+            await shell.openExternal('https://github.com/neural-dsl/neural-dsl/wiki');
+          }
+        },
+        {
+          label: 'Check for Updates',
+          click: () => {
+            checkForUpdates(true);
+          }
+        },
+        { type: 'separator' },
+        {
+          label: 'About',
+          click: () => {
+            dialog.showMessageBox(mainWindow, {
+              type: 'info',
+              title: 'About Neural Aquarium',
+              message: 'Neural Aquarium',
+              detail: `Version: ${app.getVersion()}\n\nVisual IDE for Neural DSL with integrated debugger.\n\n© 2024 Neural DSL Team`
+            });
+          }
+        }
+      ]
+    }
+  ];
 
-app.whenReady().then(() => {
-  aquarium.initialize();
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
+
+function checkForUpdates(manual = false) {
+  if (isDev) {
+    if (manual) {
+      dialog.showMessageBox(mainWindow, {
+        type: 'info',
+        title: 'Updates',
+        message: 'Update checking is disabled in development mode.'
+      });
+    }
+    return;
+  }
+
+  autoUpdater.checkForUpdates().catch((err) => {
+    log.error('Error checking for updates:', err);
+    if (manual) {
+      dialog.showErrorBox('Update Check Failed', 'Failed to check for updates. Please try again later.');
+    }
+  });
+}
+
+autoUpdater.on('checking-for-update', () => {
+  log.info('Checking for updates...');
+});
+
+autoUpdater.on('update-available', (info) => {
+  log.info('Update available:', info);
+  mainWindow.webContents.send('update-available', info);
+  
+  dialog.showMessageBox(mainWindow, {
+    type: 'info',
+    title: 'Update Available',
+    message: `A new version (${info.version}) is available!`,
+    detail: 'The update will be downloaded in the background. You will be notified when it is ready to install.',
+    buttons: ['OK']
+  });
+});
+
+autoUpdater.on('update-not-available', (info) => {
+  log.info('Update not available:', info);
+  mainWindow.webContents.send('update-not-available', info);
+});
+
+autoUpdater.on('error', (err) => {
+  log.error('Error in auto-updater:', err);
+  mainWindow.webContents.send('update-error', err);
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+  const logMessage = `Download speed: ${progressObj.bytesPerSecond} - Downloaded ${progressObj.percent}% (${progressObj.transferred}/${progressObj.total})`;
+  log.info(logMessage);
+  mainWindow.webContents.send('update-download-progress', progressObj);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+  log.info('Update downloaded:', info);
+  mainWindow.webContents.send('update-downloaded', info);
+
+  dialog.showMessageBox(mainWindow, {
+    type: 'info',
+    title: 'Update Ready',
+    message: 'A new update has been downloaded.',
+    detail: 'The application will restart to apply the update.',
+    buttons: ['Restart Now', 'Later']
+  }).then((result) => {
+    if (result.response === 0) {
+      autoUpdater.quitAndInstall(false, true);
+    }
+  });
+});
+
+ipcMain.handle('check-for-updates', () => {
+  checkForUpdates(true);
+});
+
+ipcMain.handle('install-update', () => {
+  autoUpdater.quitAndInstall(false, true);
+});
+
+ipcMain.handle('get-app-version', () => {
+  return app.getVersion();
+});
+
+app.on('ready', () => {
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
 process.on('uncaughtException', (error) => {
-  console.error('Uncaught exception:', error);
-});
-
-process.on('unhandledRejection', (error) => {
-  console.error('Unhandled rejection:', error);
+  log.error('Uncaught exception:', error);
 });
--- neural/aquarium/electron/preload.js
+++ neural/aquarium/electron/preload.js
@@ -1,99 +1,43 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-const API = {
-  project: {
-    create: (projectData) => ipcRenderer.invoke('project:create', projectData),
-    open: () => ipcRenderer.invoke('project:open'),
-    save: (projectData) => ipcRenderer.invoke('project:save', projectData),
-    close: () => ipcRenderer.invoke('project:close'),
-    getRecent: () => ipcRenderer.invoke('project:get-recent'),
+contextBridge.exposeInMainWorld('electron', {
+  checkForUpdates: () => ipcRenderer.invoke('check-for-updates'),
+  installUpdate: () => ipcRenderer.invoke('install-update'),
+  getAppVersion: () => ipcRenderer.invoke('get-app-version'),
+  
+  onUpdateAvailable: (callback) => {
+    ipcRenderer.on('update-available', (event, info) => callback(info));
   },
-
-  file: {
-    open: () => ipcRenderer.invoke('file:open'),
-    save: (filePath, content) => ipcRenderer.invoke('file:save', filePath, content),
-    saveAs: (content) => ipcRenderer.invoke('file:save-as', content),
-    read: (filePath) => ipcRenderer.invoke('file:read', filePath),
+  
+  onUpdateNotAvailable: (callback) => {
+    ipcRenderer.on('update-not-available', (event, info) => callback(info));
   },
-
-  dsl: {
-    parse: (dslCode) => ipcRenderer.invoke('dsl:parse', dslCode),
-    compile: (dslCode, backend, options) => ipcRenderer.invoke('dsl:compile', dslCode, backend, options),
-    validate: (dslCode) => ipcRenderer.invoke('dsl:validate', dslCode),
-    getExamples: () => ipcRenderer.invoke('dsl:get-examples'),
+  
+  onUpdateDownloadProgress: (callback) => {
+    ipcRenderer.on('update-download-progress', (event, progress) => callback(progress));
   },
-
-  execution: {
-    run: (script, options) => ipcRenderer.invoke('execution:run', script, options),
-    stop: (executionId) => ipcRenderer.invoke('execution:stop', executionId),
-    getStatus: (executionId) => ipcRenderer.invoke('execution:get-status', executionId),
-    onOutput: (callback) => {
-      ipcRenderer.on('execution:output', (event, data) => callback(data));
-      return () => ipcRenderer.removeListener('execution:output', callback);
-    },
-    onMetrics: (callback) => {
-      ipcRenderer.on('execution:metrics', (event, data) => callback(data));
-      return () => ipcRenderer.removeListener('execution:metrics', callback);
-    },
+  
+  onUpdateDownloaded: (callback) => {
+    ipcRenderer.on('update-downloaded', (event, info) => callback(info));
   },
-
-  debugger: {
-    start: (config) => ipcRenderer.invoke('debugger:start', config),
-    stop: () => ipcRenderer.invoke('debugger:stop'),
-    setBreakpoint: (file, line) => ipcRenderer.invoke('debugger:set-breakpoint', file, line),
-    removeBreakpoint: (file, line) => ipcRenderer.invoke('debugger:remove-breakpoint', file, line),
-    step: (type) => ipcRenderer.invoke('debugger:step', type),
-    continue: () => ipcRenderer.invoke('debugger:continue'),
-    getVariables: () => ipcRenderer.invoke('debugger:get-variables'),
-    onStateChange: (callback) => {
-      ipcRenderer.on('debugger:state-change', (event, data) => callback(data));
-      return () => ipcRenderer.removeListener('debugger:state-change', callback);
-    },
+  
+  onUpdateError: (callback) => {
+    ipcRenderer.on('update-error', (event, error) => callback(error));
   },
-
-  python: {
-    getVersion: () => ipcRenderer.invoke('python:get-version'),
-    getPath: () => ipcRenderer.invoke('python:get-path'),
-    install: (packageName) => ipcRenderer.invoke('python:install', packageName),
-    listPackages: () => ipcRenderer.invoke('python:list-packages'),
+  
+  onMenuNewProject: (callback) => {
+    ipcRenderer.on('menu-new-project', () => callback());
   },
-
-  backend: {
-    connect: () => ipcRenderer.invoke('backend:connect'),
-    disconnect: () => ipcRenderer.invoke('backend:disconnect'),
-    getStatus: () => ipcRenderer.invoke('backend:get-status'),
-    send: (message) => ipcRenderer.invoke('backend:send', message),
-    onMessage: (callback) => {
-      ipcRenderer.on('backend:message', (event, data) => callback(data));
-      return () => ipcRenderer.removeListener('backend:message', callback);
-    },
+  
+  onMenuOpenProject: (callback) => {
+    ipcRenderer.on('menu-open-project', () => callback());
   },
-
-  config: {
-    get: (key) => ipcRenderer.invoke('config:get', key),
-    set: (key, value) => ipcRenderer.invoke('config:set', key, value),
-    getAll: () => ipcRenderer.invoke('config:get-all'),
-    reset: () => ipcRenderer.invoke('config:reset'),
+  
+  onMenuSave: (callback) => {
+    ipcRenderer.on('menu-save', () => callback());
   },
-
-  window: {
-    minimize: () => ipcRenderer.invoke('window:minimize'),
-    maximize: () => ipcRenderer.invoke('window:maximize'),
-    close: () => ipcRenderer.invoke('window:close'),
-    toggleFullScreen: () => ipcRenderer.invoke('window:toggle-fullscreen'),
-  },
-
-  dialog: {
-    showError: (title, message) => ipcRenderer.invoke('dialog:show-error', title, message),
-    showInfo: (title, message) => ipcRenderer.invoke('dialog:show-info', title, message),
-    showWarning: (title, message) => ipcRenderer.invoke('dialog:show-warning', title, message),
-    showConfirm: (title, message) => ipcRenderer.invoke('dialog:show-confirm', title, message),
-  },
-
-  shell: {
-    openExternal: (url) => ipcRenderer.invoke('shell:open-external', url),
-    openPath: (path) => ipcRenderer.invoke('shell:open-path', path),
-  },
-};
-
-contextBridge.exposeInMainWorld('aquarium', API);
+  
+  removeAllListeners: (channel) => {
+    ipcRenderer.removeAllListeners(channel);
+  }
+});
